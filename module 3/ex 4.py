0116b1a7a954e51823f89f4af39e759d36f40800
masha <mariia.Atamanova@metropolia.fi>
2024-08-29 14:34:50 +0300

    module 4

--- module 3/ex 4.py
+++ module 3/ex 4.py
@@ -1,13 +1,9 @@
 #4
 year = int(input("Enter your year: "))
-if (year % 4 ==0):
-    print ('Your year is a leap year')
-elif (year%100 ==0 and year%400 ==0):
-    print ("Your year is leap year")
+
+if (year%100==0 and year % 400==0):
+    print ("Your year is a leap year")
+elif year % 4 == 0:
+    print ("Your year is a leap year")
 else :
-    print ("Your year is odd")
-
-
-
-
-
+    print ("Your year is not leap")
