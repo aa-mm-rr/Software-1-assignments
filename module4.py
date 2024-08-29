981c58df8531d49626164bc7d3feb599e259b642
masha <mariia.Atamanova@metropolia.fi>
2024-08-29 12:46:49 +0300

    final changes

--- module4.py
+++ module4.py
@@ -44,3 +44,22 @@ while first<5:
     first += 1
     print('all done')
 '''
+#1
+number = 1
+while number <=1000:
+    if number % 3 == 0:
+        print(number)
+    number+=1
+
+#2
+while True:
+    inches = float(input('Enter the number of inches: '))
+    if inches<0:
+        print('program stopped')
+        break
+    cm = inches * 2.54
+    print(f'{inches} is equal to {cm} centimeters')
+
+#3
+
+
