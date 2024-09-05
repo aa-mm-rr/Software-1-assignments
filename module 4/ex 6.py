686bb2b6218266accb9535e345082929eec9d037
masha <mariia.Atamanova@metropolia.fi>
2024-09-05 14:54:16 +0300

    new

--- module 4/ex 6.py
+++ module 4/ex 6.py
@@ -4,8 +4,8 @@ inCircle = 0
 counter = 0
 
 while counter < points:
-    xOfPoints = float(random.uniform( -1, 1))
-    yOfPoints = float(random.uniform( -1, 1))
+    xOfPoints = random.uniform( -1, 1)
+    yOfPoints = random.uniform( -1, 1)
     if xOfPoints**2 + yOfPoints**2 < 1:
         inCircle += 1
     counter += 1
