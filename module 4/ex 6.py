a5e6a74ec41e1a24758728b491aac7aeef1ee935
masha <mariia.Atamanova@metropolia.fi>
2024-09-05 14:50:50 +0300

    new

---
+++ module 4/ex 6.py
@@ -0,0 +1,14 @@
+import random
+points = int(input("How many points do you want? "))
+inCircle = 0
+counter = 0
+
+while counter < points:
+    xOfPoints = float(random.uniform( -1, 1))
+    yOfPoints = float(random.uniform( -1, 1))
+    if xOfPoints**2 + yOfPoints**2 <= 1:
+        inCircle += 1
+    counter += 1
+
+pi = 4 * inCircle / points
+print(pi)
