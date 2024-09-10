d0b3b01ecd7de1c4e3ab13a0efe2e76a07987784
masha <mariia.Atamanova@metropolia.fi>
2024-09-10 16:10:10 +0300

    module 7

--- module 7/in-class exercises.py
+++ module 7/in-class exercises.py
@@ -1,1 +1,11 @@
-
+'''
+dict1 = {'m': 11, 'f':34,'h':77}
+print(dict1.values())
+'''
+dict2 = {}
+for _ in range(5):
+    key = input('Enter a key: ')
+    value = int(input('Enter a value: '))
+    dict2[key] = value
+totalSum = sum(dict2.values())
+print('Total sum of all entered values is: ', totalSum)
