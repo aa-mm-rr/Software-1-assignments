fcc0952a391a62c43a3026dad2d1bb2f28dd90d6
masha <mariia.Atamanova@metropolia.fi>
2024-09-10 09:11:26 +0300

    module 5

---
+++ module 5/ex 3.py
@@ -0,0 +1,14 @@
+def prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+number = int(input("Enter your number: "))
+
+if prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
